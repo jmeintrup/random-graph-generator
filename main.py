d9c234e4541b3d45f36f8ac578651c911a42d5aa
Johannes Meintrup <johannes.meintrup@mni.thm.de>
2021-08-18 11:29:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,48 +19,64 @@ args = parser.parse_args()
 random.seed(args.s)
 
 def erdos(n, p):
-    edges=[]
+    edges=random_spanning_tree(n)
     for v in range(1,n+1):
         for u in range(v+1, n+1):
             if random.random() <= p:
-                edges.append((v, u))
-    return edges
-
-def fixed(n, m):
-    max_edges = (n*(n-1))/2
-    if m > max_edges:
-        return None
-    edges=set()
-    if m <= max_edges/2:
-        while len(edges) < m:
-            v=random.randint(1, n)
-            u=random.randint(1, n)
-            if v == u:
-                continue
-            edges.add((min(u, v), max(u, v)))
-    else:
-        for v in range(1, n+1):
-            for u in range(v+1, n+1):
                 edges.add((v, u))
-        while len(edges) > m:
-            v=random.randint(1, n)
-            u=random.randint(1, n)
-            edges.discard((u, v))
     edges=list(edges)
     edges.sort()
     return edges
 
+def fixed(n, m):
+    spanning_tree=random_spanning_tree(n)
+    edges=spanning_tree
+    if len(spanning_tree)>m:
+        return spanning_tree
+    all_edges=set()
+    for u in range(0,n):
+        for v in range(u+1,n):
+            all_edges.add((u, v))
+    candidates=all_edges-spanning_tree
+    while len(edges) < m and len(candidates) > 0:
+        edge = random.sample(candidates, 1).pop()
+        candidates.remove(edge)
+        edges.add(edge)
+    edges=list(edges)
+    edges.sort()
+    return edges
+    
+
+def random_spanning_tree(n):
+    vertices=list(range(0,n))
+    S=set(vertices)
+    T=set()
+    v = random.sample(S, 1).pop()
+    S.remove(v)
+    T.add(v)
+
+    edges=set()
+    while len(S)>0:
+        u=random.sample(vertices, 1).pop()
+        if u not in T:
+            edges.add((min(u,v), max(u,v)))
+            S.remove(u)
+            T.add(u)
+        v=u
+    return edges
 
 def print_dimacs(n, edges, comment=None):
     print(f"p edges {n} {len(edges)}")
     for edge in edges:
-        print(f"e {edge[0]} {edge[1]}")
+        print(f"e {edge[0]+1} {edge[1]+1}")
 
 if args.type == 'erdos':
     edges=erdos(args.n, args.p)
     print(f"c Randomly Generated Erdos-Reyni Graph with p={args.p} and n={args.n}")
     print_dimacs(args.n, edges)
 if args.type == 'fixed':
+    if args.m == None:
+        raise argparse.ArgumentTypeError('Fixed type requires "m" argument')
     print(f"c Randomly Generated graph with fixed number of edges and vertices")
     edges=fixed(args.n, args.m)
     print_dimacs(args.n, edges)
